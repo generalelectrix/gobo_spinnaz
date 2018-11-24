41187fef39be3d4eb24a833ed8a5f5eda91807a8
general electrix <general.electrix@gmail.com>
2018-11-23 16:58:07 -0800

    Rebuild project to use DMXSerial library.

--- Types.h
+++
@@ -1,9 +0,0 @@
-#ifndef Types_h
-#define Types_h
-
-struct MotorState {
-  uint8_t direction;
-  uint8_t speed;
-};
-
-#endif
--- gobo_spinnaz.ino
+++ gobo_spinnaz.ino
@@ -15,30 +15,9 @@
 * second is speed.
 *
 */
-#include "Conceptinetics/Conceptinetics.h"
-#include <Wire.h>
+#include "DMXSerial.h"
 #include "Adafruit_MotorShield.h"
 #include "Adafruit_MS_PWMServoDriver.h"
-#include "Types.h"
-
-//
-// CTC-DRA-13-1 ISOLATED DMX-RDM SHIELD JUMPER INSTRUCTIONS
-//
-// If you are using the above mentioned shield you should
-// place the RXEN jumper towards G (Ground), This will turn
-// the shield into read mode without using up an IO pin
-//
-// The !EN Jumper should be either placed in the G (GROUND)
-// position to enable the shield circuitry
-//   OR
-// if one of the pins is selected the selected pin should be
-// set to OUTPUT mode and set to LOGIC LOW in order for the
-// shield to work
-//
-
-#define MOTOR_COUNT 4
-#define CHAN_PER_MOTOR 2
-#define DMX_CHANNEL_COUNT (MOTOR_COUNT * CHAN_PER_MOTOR)
 
 // may change this to an external LED at some point
 #define LED_PIN LED_BUILTIN
@@ -55,16 +34,12 @@ void blink (uint8_t times, uint16_t duration)
     }
 }
 
-// DMX slave receiver
-DMX_Slave dmxSlave(DMX_CHANNEL_COUNT);
-
 // setting this to -1 runs in standalone
 int16_t dmxAddress;
 
-// keep track of when we last got a DMX frame
-// we'll blink the LED if we have good DMX coming in
-// we'll stop blinking the LED if we haven't seen DMX in 1 second
-volatile uint32_t lastFrameReceivedTime = 0ul;
+// Keep track of when we last got a DMX frame.
+// Blink the LED if we have good DMX coming in.
+// Stop blinking the LED if we haven't seen DMX in dmxTimeout ms.
 const uint16_t dmxTimeout = 1000u;
 
 // Small state machine that blinks the LED if we're getting good DMX.
@@ -76,8 +51,9 @@ const uint16_t blinkDuration = 500;  // Nice, slow blinking.
 void serviceLedState ()
 {
     uint32_t now = millis();
+    uint32_t timeSinceLastPacket = DMXSerial.noDataSince();
     // If we haven't seen a frame in a while, shut LED off.
-    if ((lastFrameReceivedTime + dmxTimeout) < now)
+    if (timeSinceLastPacket > dmxTimeout)
     {
         digitalWrite(LED_PIN, LOW);
     }
@@ -124,12 +100,6 @@ uint8_t speedFromDigit (int16_t pin)
     return standaloneValues[readDigitEntry(pin)];
 }
 
-// stored state of each motor
-volatile MotorState motor0State = MotorState {FORWARD, 0};
-volatile MotorState motor1State = MotorState {FORWARD, 0};
-volatile MotorState motor2State = MotorState {FORWARD, 0};
-volatile MotorState motor3State = MotorState {FORWARD, 0};
-
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield motorShield = Adafruit_MotorShield();
 
@@ -139,23 +109,28 @@ Adafruit_DCMotor *motor1 = motorShield.getMotor(2);
 Adafruit_DCMotor *motor2 = motorShield.getMotor(3);
 Adafruit_DCMotor *motor3 = motorShield.getMotor(4);
 
-// Use a MotorState to set the state of a motor.
-void pushMotorState (Adafruit_DCMotor* motor, MotorState* motorState)
+// Set speed and direction for a given motor.
+void setMotorState (Adafruit_DCMotor* motor, uint8_t speed, uint8_t direction)
 {
-    motor->setSpeed(motorState->speed);
-    motor->run(motorState->direction);
+    motor->setSpeed(speed);
+    motor->run(direction);
 }
 
-// Push state to all of the motors.
-void pushMotorStates ()
+// Given a channel offset, set motor state from DMX values.
+void setMotorStateFromDmx (Adafruit_DCMotor* motor, uint16_t channelOffset)
 {
-    // disable interrupts while we're reading the motor states
-    noInterrupts();
-    pushMotorState(motor0, &motor0State);
-    pushMotorState(motor1, &motor1State);
-    pushMotorState(motor2, &motor2State);
-    pushMotorState(motor3, &motor3State);
-    interrupts();
+    uint16_t startChannel = dmxAddress + channelOffset;
+    // Speed on second channel.
+    uint8_t speed = DMXSerial.read(startChannel + 1);
+    // First channel is direction, forward if less than 127.
+    if (DMXSerial.read(startChannel) < 128)
+    {
+        setMotorState(motor, speed, FORWARD);
+    }
+    else
+    {
+        setMotorState(motor, speed, BACKWARD);
+    }
 }
 
 void setup ()
@@ -205,7 +180,10 @@ void setup ()
     motorShield.begin();  // create with the default frequency 1.6KHz
     //motorShield.begin(1000);  // OR with a different frequency, say 1KHz
 
-    pushMotorStates();
+    setMotorState(motor0, 0, FORWARD);
+    setMotorState(motor1, 0, FORWARD);
+    setMotorState(motor2, 0, FORWARD);
+    setMotorState(motor3, 0, FORWARD);
 
     motor0->run(RELEASE);
     motor1->run(RELEASE);
@@ -215,11 +193,8 @@ void setup ()
     // if we're not in standalone, configure DMX
     if (dmxAddress > 0)
     {
-        // Enable DMX slave interface and start listening to DMX data.
-        dmxSlave.enable();
-        dmxSlave.setStartAddress(dmxAddress);
-        // Register the interrupt handler to run on frame completion.
-        dmxSlave.onReceiveComplete(handleDmxFrame);
+        DMXSerial.maxChannel(512);
+        DMXSerial.init(DMXReceiver);
     }
 }
 
@@ -228,54 +203,21 @@ void loop ()
     // If we're in standalone mode, read the control values and set state.
     if (-1 == dmxAddress)
     {
-        motor0State.speed = speedFromDigit(A0);
-        motor1State.speed = speedFromDigit(A1);
-        motor2State.speed = speedFromDigit(A2);
-        motor3State.speed = speedFromDigit(A3);
+        setMotorState(motor0, speedFromDigit(A0), FORWARD);
+        setMotorState(motor1, speedFromDigit(A1), FORWARD);
+        setMotorState(motor2, speedFromDigit(A2), FORWARD);
+        setMotorState(motor3, speedFromDigit(A3), FORWARD);
     }
-    // If we're running in DMX mode, blink the LED if we have good signal.
+    // If we're running in DMX mode, read values and update LED indicator.
     else
     {
+        setMotorStateFromDmx(motor0, 0);
+        setMotorStateFromDmx(motor1, 2);
+        setMotorStateFromDmx(motor2, 4);
+        setMotorStateFromDmx(motor3, 6);
         serviceLedState();
     }
 
-    // Render the motor states to the motor controller.
-    pushMotorStates();
-
     // Run at ~50 fps.
     delay(20);
-}
-
-// Given a channel offset, set the motor state from DMX values.
-void setMotorStateFromDmx (uint16_t channelOffset, MotorState* motorState)
-{
-    // First channel is direction, forward if less than 127.
-    if (dmxSlave.getChannelValue(channelOffset + 1) > 127)
-    {
-        motorState->direction = BACKWARD;
-    }
-    else
-    {
-        motorState->direction = FORWARD;
-    }
-    // Second channel is speed.
-    motorState->speed = dmxSlave.getChannelValue(channelOffset + 2);
-}
-
-// Interrupt handler for when the DMX interface has read a frame.
-void handleDmxFrame (uint16_t channelsReceived)
-{
-    // Possible we didn't receive all of the channels, such as a truncated DMX universe.
-    // Only update state if we got all of them.
-    if (DMX_CHANNEL_COUNT == channelsReceived)
-    {
-        // Update the stored motor state.
-        setMotorStateFromDmx(0, &motor0State);
-        setMotorStateFromDmx(2, &motor1State);
-        setMotorStateFromDmx(4, &motor2State);
-        setMotorStateFromDmx(6, &motor3State);
-
-        // Update receive time to determine signal timeout.
-        lastFrameReceivedTime = millis();
-    }
 }
